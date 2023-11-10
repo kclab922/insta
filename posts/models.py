da31be464082870a989150bff91e8ce21d52bc97
HaeunChoi <kclab922@gmail.com>
2023-11-10 10:10:26 +0900

    like 기능을 위한 모델링

--- posts/models.py
+++ posts/models.py
@@ -14,3 +14,7 @@ class Post(models.Model):
         upload_to='image/%Y/%m'
     )
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # related_name => Post와 User를 연결하는 과정에서 1:N, M:N으로 두 번 연결됨
+    # => User 쪽에 자동생성되는 post_set도 2개가 만들어짐 -> 이름 중복되므로 하나 변경
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
+
