90811e7856f0fdd34cd086cff9eea1af63a3f9fa
HaeunChoi <kclab922@gmail.com>
2023-11-10 10:46:37 +0900

    like 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -40,3 +40,23 @@ def delete(request, id):
         post.delete()
     
     return redirect('posts:index')
+
+@login_required
+def likes(request, id):
+    # 현재 로그인한 사람의 정보 저장
+    user = request.user
+    post = Post.objects.get(id=id)
+
+    # 이미 좋아요 버튼이 눌려있는 경우
+    # if post in user.like_posts.all():
+    if user in post.like_users.all():
+        post.like_users.remove(user)
+        
+    # 좋아요 버튼을 아직 안 누른 경우
+    else:
+        # 게시물에.좋아요버튼누른사람 컬럼에 .현재 로그인한사람을 추가하는 과정
+        # 둘중 하나만 추가해도 연동이 되어있으므로 ...  어쨌든 user 랑 post에 각각 할당되는 것
+        # user.like_posts.add(post)
+        post.like_users.add(user)
+    
+    return redirect('posts:index')
